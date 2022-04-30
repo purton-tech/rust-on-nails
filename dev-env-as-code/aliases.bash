84afe2375b19950e11f2d30705aedfd3e18b492b
Ian Purton <ian.purton@gmail.com>
2022-04-30 17:18:04 +0000

    fix(devcontainer): Remove gitsetup

--- dev-env-as-code/aliases.bash
+++ dev-env-as-code/aliases.bash
@@ -7,7 +7,6 @@ alias gcam='git commit -a -m'
 alias gpsup="git push --set-upstream origin $(git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||')"
 #alias gpsup='git push --set-upstream origin $(git_current_branch)'
 alias gcb='git checkout -b'
-alias gitsetup='git config --global user.name \$NAME && git config --global user.email \$EMAIL && mkdir -p ~/.ssh && cp -u /home/host-ssh/id_rsa ~/.ssh && chmod 600 ~/.ssh/id_rsa && ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub'
 alias gcr='f() { git checkout -b $1 origin/$1; }; f'
 
 # Cargo watch
