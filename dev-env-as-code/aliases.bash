82bb6f18a8117bc11b1f673ec768f161d82c5467
Ian Purton <ian.purton@gmail.com>
2022-04-30 17:54:50 +0000

    fix(devcontainer): Remove commented out alias

--- dev-env-as-code/aliases.bash
+++ dev-env-as-code/aliases.bash
@@ -5,7 +5,6 @@ alias c=clear
 alias gp='git push'
 alias gcam='git commit -a -m'
 alias gpsup="git push --set-upstream origin $(git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||')"
-#alias gpsup='git push --set-upstream origin $(git_current_branch)'
 alias gcb='git checkout -b'
 alias gcr='f() { git checkout -b $1 origin/$1; }; f'
 
