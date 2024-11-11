e8a00a4a0de4c011061fdff49de64c58b66ef9c9
Your Name <you@example.com>
2024-11-11 14:57:40 +0000

    fix: Split the dockers

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 echo "Updating files..."
 sed -i "s/repo_version = .*$/repo_version =  \"Github $1\"/" ./rust-on-nails.com/content/_index.fi.md
 sed -i "s/repo_version = .*$/repo_version =  \"Github $1\"/" ./rust-on-nails.com/content/_index.md
-sed -i "s/purtontech\/rust-on-nails-devcontainer:.*$/purtontech\/rust-on-nails-devcontainer:$1 AS development/" ./nails-devcontainer/.devcontainer/Dockerfile
+sed -i "s/purtontech\/rust-on-nails-devcontainer-amd64:.*$/purtontech\/rust-on-nails-devcontainer-amd64:$1 AS development/" ./nails-devcontainer/.devcontainer/Dockerfile
 
 git add .
 git commit -am "chore(deployment): Update files with new version $1"
